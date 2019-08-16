1cb60ab3b5c5418f5b253155d191884e99b21f4f
kevin <kevin_zkc@126.com>
2019-08-16 13:58:32 +0800

    此版本确实通过了 springcloud配置中服务端和 客户端测试

--- microservicecloud-config-3344/src/main/java/com/atguigu/springcloud/Config_3344_StartSpringCloudApp.java
+++ microservicecloud-config-3344/src/main/java/com/atguigu/springcloud/Config_3344_StartSpringCloudApp.java
@@ -2,10 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
+//@EnableDiscoveryClient
 public class Config_3344_StartSpringCloudApp {
     public static void main(String[] args) {
         SpringApplication.run(Config_3344_StartSpringCloudApp.class, args);
--- microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/rest/ConfigClientRest.java
+++ microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/rest/ConfigClientRest.java
@@ -9,17 +9,18 @@ public class ConfigClientRest {
     @Value("${spring.application.name}")
     private String applicationName;
 
-//    @Value("${eureka.client.service-url.defaultZone}")
-    private String eurekaServers;
+   @Value("${eureka.client.service-url.defaultZone}")
+   private String eurekaServers;
 
     @Value("${server.port}")
     private String port;
 
+
     @RequestMapping("/config")
     public String getConfig() {
-        String str = "applicationname" + applicationName + "\t eurekaServers:"  + "\t port:" + port;
+        String str = "applicationname" + applicationName+"  端口："+port+"eureka集群地址："+eurekaServers;
         System.out.println("配置是：" + str);
-        return "applicationname" + applicationName + "\t eurekaServers:"  + "\t port:" + port;
+        return str;
     }
 
 }
